a25d4543540b0e5d61c218b23b9408c4944fbb97
Jainil <jpats1028@gmail.com>
2021-09-20 14:42:39 -0400

    Revise

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,7 @@
+import React from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { BrowserRouter as Router, Route, Link} from 'react-router-dom'
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
@@ -10,10 +11,6 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          
-          <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
-          
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -22,13 +19,13 @@ function App() {
           >
             Learn React
           </a>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
         </header>
-        <body>
-          <div>
-              <Route exact path="/" component={Fib} />
-              <Route path="/otherpage" componen={OtherPage} />
-            </div>
-        </body>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
+        </div>
       </div>
     </Router>
   );
--- client/src/OtherPage.js
+++ client/src/OtherPage.js
@@ -2,10 +2,10 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 export default () => {
-    return (
-        <div>
-          Im some other div!
-          <Link to="/">Go Home</Link>  
-        </div>
-    );
+  return (
+    <div>
+      Im some other page!
+      <Link to="/">Go back home</Link>
+    </div>
+  );
 };
